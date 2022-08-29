455369ded65d9ed590be7fffc1024d7d0f34d62f
Robin Barwari <70271799+Robin-Barwari@users.noreply.github.com>
2022-08-29 14:08:00 +0200

    test

--- Repos/Project_WEB_1/src/App.vue
+++ Repos/Project_WEB_1/src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="135" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="No!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
--- Repos/Project_WEB_1/src/components/HelloWorld.vue
+++ Repos/Project_WEB_1/src/components/HelloWorld.vue
@@ -9,11 +9,9 @@ defineProps({
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
+    <h1 class="green">{{  msg  }}</h1>
     <h3>
-      You’ve successfully created a project with
-      <a target="_blank" href="https://vitejs.dev/">Vite</a> +
-      <a target="_blank" href="https://vuejs.org/">Vue 3</a>.
+      No!
     </h3>
   </div>
 </template>
@@ -35,6 +33,7 @@ h3 {
 }
 
 @media (min-width: 1024px) {
+
   .greetings h1,
   .greetings h3 {
     text-align: left;
