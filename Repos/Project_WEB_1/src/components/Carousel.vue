a3b0ea83eed2b676d0edbcbe98c1734b05e01345
Rahim Ahmed <rahimo.ahmedo@gmail.com>
2022-08-30 21:31:25 +0200

    carousel

--- Repos/Project_WEB_1/src/components/Carousel.vue
+++ Repos/Project_WEB_1/src/components/Carousel.vue
@@ -3,6 +3,7 @@
         <div class="carousel-inner">
             <carousel-item v-for="(slide, index) in slides" :slide="slide" :key="`item-${index}`"
                 :current-slide="currentSlide" :index="index"></carousel-item>
+            <carousel-controls @prev="prev" @next="next"></carousel-controls>
         </div>
     </div>
 </template>
@@ -11,23 +12,31 @@
 
 <script>
 import CarouselItem from './CarouselItem.vue';
+import CarouselControls from './CarouselControls.vue';
 
 export default {
     props: ['slides'],
-    components: { CarouselItem },
+    components: { CarouselItem, CarouselControls },
     data: () => ({
-        currentSlide: 0,
+        currentSlide: 1,
         slideInterval: null
     }),
     methods: {
         setCurrentSlide(index) {
             this.currentSlide = index;
         },
+        prev() {
+            const index = this.currentSlide > 0 ? this.currentSlide - 1 : this.slides.Length - 1;
+            this.setCurrentSlide(index);
+        },
+        next() {
+            const index = this.currentSlide < this.slides.Length - 1 ? this.currentSlide + 1 : 0;
+            this.setCurrentSlide(index);
+        }
     },
     mounted() {
         this.slideInterval = setInterval(() => {
-            const index = this.currentSlide < this.slides.Length - 1 ? this.currentSlide + 1 : 0;
-            this.setCurrentSlide(index);
+            this.next();
         }, 3000)
     },
     beforeUnmount() {
---
+++ Repos/Project_WEB_1/src/components/CarouselControls.vue
@@ -0,0 +1,32 @@
+<template>
+    <button class="carousel-control left" @click="$emit('prev')">Prev</button>
+    <button class="carousel-control right" @click="$emit('next')">Next</button>
+</template>
+
+<script>
+export default {
+    emits: ['prev', 'next']
+}
+</script>
+
+<style scoped>
+.carousel-control {
+    background-color: black;
+    border: none;
+    display: inline-block;
+    position: absolute;
+    height: 50px;
+    width: 70px;
+    top: calc(50% - 25px);
+    color: white;
+    cursor: pointer;
+}
+
+.left {
+    left: 0;
+}
+
+.right {
+    right: 0;
+}
+</style>
